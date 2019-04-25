486fa1120465d2f29b59f672fcf5df1f07b2d57f
abernauer <bernaue2@illinois.edu>
2019-04-25 22:47:38 +0000

    four compiler errors

--- PCA_R_BINDING.cpp
+++ PCA_R_BINDING.cpp
@@ -20,25 +20,37 @@
 //'@param var to retain a double Amount of variance to retain falling between 0 and 1 defaults to 0
 //'@return A matrix with PCA performed on it
 // [[Rcpp::depends(RcppArmadillo)]]
-Rcpp::as<arma::mat> input;
-string decomposition_method = 'exact';
-const int new_dimensionality = 0;
-bool copy_all_inputs;
-bool scale;
-double var_to_retain;
-bool verbose;
+using namespace Rcpp;
 
 
-// [[Rcpp::exports]]
-Rcpp::List PCA(input,  decomposition_method, new_dimensionality, copy_all_inputs = false,  scale = false, var_to_retain = 0, verbose = false);
-{
+SEXP Y;
+NumericMatrix data(Y);
+template<>as<arma::mat>(data);
+
+std::string method = "exact";
+std::vector<double> dimensionality;
+bool copys = false;
+bool s = false;
+std::vector<double> var_r;
+bool verbos = false; 
+List PCA;
+// [[Rcpp::export]]
+List PCA = List::create(_["input"] = data, 
+		     _["decomposition_method"] =  method, 
+		     _["new_dimensionality"] =  dimensionality, 
+		     _["copy_all_inputs"] =  copys, 
+		     _["scale"] =  s, 
+		     _["var_to_retain"] =  var_r,
+		     _["verbose"] =  verbos);{ 
 // don't forget to declare argument types in line above
-ResetTimers();
-EnableTimers();
-DisableBacktrace();
-DisableVerbose();
-CLI.RestoreSettings("Principal Components Analysis");
-if (copy_all_inputs == true){
+   
+ResetTimers()
+EnableTimers()
+DisableBacktrace()
+DisableVerbose()
+CLI.RestoreSettings("Principal Components Analysis")
+if (copy_all_inputs == true)
+{
 CLI.SetParam[bool](<const string> 'copy_all_inputs', copy_all_inputs);
 
 // alternatively Rccp::as < arma::mat >(Rmat); or other R object name
@@ -50,19 +62,22 @@ del input_mat;
 }
 
 // detect if the parameter was passed; set if so.
-if (decomposition_method != none) {
+if (decomposition_method != none)
+{
 SetParam[string](<const string>'decomposition_method', decomposition_method.encode("UTF-8"));
 CLI.SetPassed(<const string>'decomposition_method');
 }
 
 // detect if the parameter was passed; set if so.
-if (new_dimensionality != 0) {
+if (new_dimensionality != 0)
+{
 SetParam[int](<const string>'new_dimensionality', new_dimensionality);
 CLI.SetPassed(<const string>'new_dimensionality');
 }
 
 //detect if the parameter was passed; set if so.
-if (scale != false){
+if (scale != false)
+{
 SetParam[bool](<const string>'scale', scale);
 CLI.SetPassed(<const string> 'scale');
 }
@@ -74,20 +89,21 @@ CLI.SetPassed(<const string> 'var_to_retain');
 }
 
 //detect if the parameter was passed; set if so.
-if (verbose != false){
+if (verbose != false)
+{
 SetParam[bool](<const string>'verbose', verbose);
 CLI.SetPassed(<const string>'verbose');
 EnableVerbose();
 }
 
 //Mark all out options as passed
-CLI.SetPassed(<const string>'verbose');
+CLI<string>.SetPassed(<const string>'verbose');
 
-mlpackMain();
+mlpackMain()
 
 
 
-CLI.ClearSettings();
+CLI.ClearSettings()
 
 //iniatilize results list as R idiom for python dict with name value pairs schematic
 //List::create is an implicit call to Rcpp::wrap() which returns an R object or converts from C++ to R types
@@ -96,8 +112,7 @@ CLI.ClearSettings();
  
 return result = List::create(Named('output') = Rcpp::as<arma::mat&>(CLI.GetParam[arma.Mat[double]], 'output'));
 }
-// ***R
-//
-//
-//
-// ***
+ ***R
+ Y = matrix(1:16, 4, 4)
+  PCA()
+ ***
